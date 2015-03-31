f946e0d3696c6a9d74d71e0b4d1578b810dd8492
threehams <threehams@gmail.com>
2015-03-31 15:00:55 -0700

    Add debug button

--- client/js/pages/page/page-controller.js
+++ client/js/pages/page/page-controller.js
@@ -1,18 +1,31 @@
 'use strict';
 
-module.exports = ['ProgressService', '$timeout', function (ProgressService, $timeout) {
+module.exports = ['ProgressService', '$location', function (ProgressService, $location) {
   var vm = this;
 
   vm.slide = 0;
   vm.getProgress = function() {
     ProgressService.index().then(function(data) {
       vm.epics = data.epics;
-      vm.totalProgress = data.total;
+      if ($location.search().progress) {
+        vm.fakeProgress = parseInt($location.search().progress);
+        vm.totalProgress = {
+          art: vm.fakeProgress,
+          code: vm.fakeProgress,
+          design: vm.fakeProgress
+        };
+      } else {
+        vm.totalProgress = data.total;
+      }
       //$timeout(vm.getProgress, 2000);
     }).catch(function(err) {
       console.log(err);
       //$timeout(vm.getProgress, 60000);
     });
+  };
+
+  vm.incrementProgress = function(progress) {
+    $location.path('/').search({progress: progress});
   };
 
   vm.getProgress();
